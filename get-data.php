f5c61dae6096d686d23428d3363fe462422ca735
Brijesh Kalkani <developer.logwintech@gmail.com>
2022-07-28 18:01:20 +0530

    Database connection done

--- get-data.php
+++ get-data.php
@@ -1,53 +1,46 @@
 <?php
-session_start();
 
 $servername = 'localhost';
 $username = 'root';
 $password = '';
-$dbname = "DB";
+$dbname = "db";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-    
 }
 
-if (isset($_POST['Submit'])) {
+if (isset($_POST['submit'])) {
 
+
+    // print_r('submit');
           $firstname = $_POST['firstname'];
           $lastname = $_POST['lastname'];
-          $Email     = $_POST['E-mail'];
-          $MobileNumber     = $_POST['Mobile Number'];
-          $Howmanyrooms = $_POST['How many rooms'];
-          $Howmanyperson = $_POST['How many person'];
-          $CheckInDate = $_POST['CheckInDate '];
-          $CheckOutDate  = $_POST['CheckOutDate '];
+          $email = $_POST['email'];
+          $rooms = $_POST['rooms'];
+          $person = $_POST['person'];
+          $mobilenumber = $_POST['mobilenumber'];
+          $checkindate = $_POST['checkindate'];
+          $checkoutdate = $_POST['checkoutdate'];
 
 
-        
 
-       $sql = " INSERT INTO `Guests`(`firstname`, `lastname`, `email`, `rooms`, `person`, `mobilenumber`, `checkindate`, `checkoutdate`) 
+       $sql = " INSERT INTO Guests(firstname, lastname, email, rooms, person, mobilenumber, checkindate,checkoutdate)
        
-       VALUES ('$firstname','$lastname','$Email','$rooms','$person','$mobilenumber','$checkindate','$checkoutdate')";
+       VALUES ('$firstname','$lastname','$email','$rooms','$person','$mobilenumber','$checkindate','$checkoutdate')";
 
     if (mysqli_query($conn, $sql)) {
+
         echo "<script>alert(New record has been added successfully !)</script>";
         
     } else {
         echo "Error: " . $sql .  mysqli_error($conn);
     }
     mysqli_close($conn);
-    header('Location: get-data.php');
+
+    // header('Location: http://127.0.0.1:5501/Reservation.html');
     
 }
-// if (isset($_SESSION['Username'])){
 
-//   echo " Your username is : " .  $_SESSION['Username'];
-//   echo  "<br> Your password is : " .$_SESSION['Password'];
-
-// } else{
-
-//        echo "Plese Login to continue !";
-
-// }
+?>
