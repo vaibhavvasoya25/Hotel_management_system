0a1a167b352e26193ee25c029656b3ec66d44717
Brijesh Kalkani <developer.logwintech@gmail.com>
2022-07-29 18:45:55 +0530

    Inserted data into database

--- get-data.php
+++ get-data.php
@@ -1,4 +1,11 @@
 <?php
+include_once("connection.php");
+ini_set('display_errors', 1);
+
+?>
+
+
+<?php
 
 $servername = 'localhost';
 $username = 'root';
@@ -10,37 +17,41 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
+// print_r($conn);
 if (isset($_POST['submit'])) {
 
 
-    // print_r('submit');
-          $firstname = $_POST['firstname'];
-          $lastname = $_POST['lastname'];
-          $email = $_POST['email'];
-          $rooms = $_POST['rooms'];
-          $person = $_POST['person'];
-          $mobilenumber = $_POST['mobilenumber'];
-          $checkindate = $_POST['checkindate'];
-          $checkoutdate = $_POST['checkoutdate'];
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
+    $email = $_POST['email'];
+    $rooms = $_POST['rooms'];
+    $person = $_POST['person'];
+    $mobilenumber = $_POST['mobilenumber'];
+    $checkindate = $_POST['checkindate'];
+    $checkoutdate = $_POST['checkoutdate'];
+
+    // echo $checkindate;
+    // $checkindate = strtotime($checkindate);
+    // echo $checkoutdate;
+    // $checkoutdate = strtotime($checkoutdate);
+   
+
+    //   var_dump($checkindate);
 
 
 
-       $sql = " INSERT INTO Guests(firstname, lastname, email, rooms, person, mobilenumber, checkindate,checkoutdate)
-       
-       VALUES ('$firstname','$lastname','$email','$rooms','$person','$mobilenumber','$checkindate','$checkoutdate')";
+    $sql = "INSERT INTO `Guests`(`firstname`, `lastname`, `email`, `rooms`, `person`, `mobilenumber`, `checkindate`,`checkoutdate`) VALUES ('$firstname','$lastname','$email','$rooms','$person','$mobilenumber', '$checkindate', '$checkoutdate')";
+
 
     if (mysqli_query($conn, $sql)) {
-
-        echo "<script>alert(New record has been added successfully !)</script>";
-        
+        header("Location: http://localhost/welcome_management/Reservation.html");
     } else {
         echo "Error: " . $sql .  mysqli_error($conn);
     }
     mysqli_close($conn);
 
-    // header('Location: http://127.0.0.1:5501/Reservation.html');
-    
 }
+
+
 
 ?>
